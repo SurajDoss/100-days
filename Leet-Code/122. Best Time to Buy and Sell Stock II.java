2ccddc11ac42d83f9aab401e33d1901cd3da7330
Suraj Doss <suraj_sebastien@yahoo.com>
2026-02-01 16:49:40 +0530

    Implement maxProfit method for stock trading

---
+++ Leet-Code/122. Best Time to Buy and Sell Stock II.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfits = 0;
+        int length = prices.length;
+        for(int i = 0; i < length-1; i++){
+            if( prices[i] < prices[i+1] ){
+                maxProfits += prices[i+1] - prices[i];
+            }
+        }
+
+        return maxProfits;
+    }
+}
