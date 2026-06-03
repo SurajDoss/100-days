3994a225f67437d169d56f369bb501f3ef7b9624
Suraj Doss <suraj_sebastien@yahoo.com>
2026-06-03 08:10:04 +0530

    Add recursive method to check palindrome

--- Leet-Code/9. Palindrome Number.java
+++ Leet-Code/9. Palindrome Number.java
@@ -17,4 +17,22 @@ class Solution {
 
         return true;
     }
+
+    public boolean isPalindromeRecursn(int x) {
+        String a = x + "";
+        return isPalindrome(0, a);
+    }
+
+    private static boolean isPalindrome(int right, String a){
+        //.statement to end
+        if( right == ( a.length() / 2 ) ){
+            return true;
+        }
+
+        //statement to break in case of not equal
+        if( a.charAt(right) != a.charAt( a.length() - right - 1 ) ){
+            return false;
+        }
+        return isPalindrome(++right, a);
+    }
 }
